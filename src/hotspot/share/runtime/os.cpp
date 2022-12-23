94011176599a24af601a27dde57ba99b04d8c3e7
Johannes <johannes.bechberger@sap.com>
2022-12-23 15:44:15 +0100

    Fix some of the issues

--- src/hotspot/share/runtime/os.cpp
+++ src/hotspot/share/runtime/os.cpp
@@ -1268,6 +1268,8 @@ bool os::is_first_C_frame(frame* fr) {
   if (old_fp < ufp) return true;
   if (old_fp - ufp > 64 * K) return true;
 
+  if (fr->sender_pc() == NULL) return true;
+
   return false;
 }
 
