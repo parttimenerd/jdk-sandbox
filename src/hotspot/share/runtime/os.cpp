c26a384b91a6dbdd16ddb2b263a50c1acb68c869
Johannes <johannes.bechberger@sap.com>
2023-01-13 13:47:40 +0100

    Fix segfault in frame::is_interpreted_frame_valid during fuzzing

--- src/hotspot/share/runtime/os.cpp
+++ src/hotspot/share/runtime/os.cpp
@@ -1107,7 +1107,6 @@ void os::print_date_and_time(outputStream *st, char* buf, size_t buflen) {
 
 // Check if pointer can be read from (4-byte read access).
 // Helps to prove validity of a not-NULL pointer.
-// Returns true in very early stages of VM life when stub is not yet generated.
 bool os::is_readable_pointer(const void* p) {
   int* const aligned = (int*) align_down((intptr_t)p, 4);
   int cafebabe = 0xcafebabe;  // tester value 1
